0ce3b37e6efa497fa53cb28ca3cdc8b3cf56d8b6
Daniel Cordova <dcordova@udtonline.com>
2023-09-11 23:21:36 +1000

    [Update] exclude test patterns config

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    excludeSpecPattern:[
+      "**/1-getting-started/*.js",
+      "**/2-advanced-examples/*.js"
+    ]
   },
 });
