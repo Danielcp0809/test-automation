2ede9262401124f3ee19e8c1d285c19b7c5fb68e
Daniel Cordova <dcordova@udtonline.com>
2023-09-12 00:13:25 +1000

    [Add] contains and parents locators

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -24,4 +24,14 @@ describe('Types of locators', () => {
     it('Should get a item by class', () => {
         cy.get('.btn'); // get all elements with class equals to btn
     });
+
+    it('Should get a item to contain a text', () => {
+        cy.contains('Reading'); // get all elements tagged by input that contains First
+        cy.contains('.header-wrapper', 'Widgets')
+    })
+
+    it('Should get a item to contain a parents', () => {
+        cy.get('#firstName').parent(); // get all elements with id equals to firstName and its parent
+        cy.get('#firstName').parents().find('label'); // get all elements with id equals to firstName and its parent and find the label element
+    })
 });
