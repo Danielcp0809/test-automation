de9820802182b92a81fa8015a5323c376b2a6fd1
Daniel Cordova <dcordova@udtonline.com>
2023-09-12 19:10:12 +1000

    [Add] Access JQuery elements tests

---
+++ cypress/e2e/savingElements.cy.js
@@ -0,0 +1,17 @@
+describe('Saving elements', () => {
+    it('Should get a item by tag', () => {
+		cy.visit('/automation-practice-form');
+        cy.get('input[placeholder="First Name"]');
+        cy.get('input[placeholder="First Name"]').parents('form').then(($el) => {
+            const inputs = $el.find('input');
+            const div = $el.find('div');
+            const labels = $el.find('label');
+
+            expect(inputs.length).to.equal(15);
+            expect(div.length).to.equal(70);
+            expect(labels.length).to.equal(16);
+            
+            cy.wrap(inputs).should('have.length', 15) // using wrap, we can use cypress commands passing a JQuery element
+        })
+	});
+});
